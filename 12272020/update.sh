c8aeb9316fbe1ffbe3012c5480416bc8c4e1ba81
ridgek <ridgekuhn@users.noreply.github.com>
2020-12-27 16:35:05 -0500

    * Make cloud sync scripts executable * Replace stock retroarch.cfg files with backup of user's * Add update lock file

--- 12272020/update.sh
+++ 12272020/update.sh
@@ -19,11 +19,21 @@ if [ ! -f "/home/ark/.config/.update${RIDGEK_DATE}" ]; then
 	# Install update payload
 	sudo wget ${RIDGEK_URL}/arkosupdate${RIDGEK_DATE}.zip -O /home/ark/arkosupdate${RIDGEK_DATE}.zip -a "$LOG_FILE"
 	if [ -f "/home/ark/arkosupdate${RIDGEK_DATE}.zip" ]; then
+		# Backup user's retroarch.cfg files
+		# @todo Remove this if option to select retroarch save location becomes available
+		cp -v /home/ark/.config/retroarch/retroarch.cfg /home/ark/.config/retroarch/retroarch.cfg.bak | tee -a "$LOG_FILE"
+		cp -v /home/ark/.config/retroarch32/retroarch.cfg /home/ark/.config/retroarch32/retroarch.cfg.bak | tee -a "$LOG_FILE"
+
+		# Payload
 		sudo unzip -X -o /home/ark/arkosupdate${RIDGEK_DATE}.zip -d / | tee -a "$LOG_FILE"
 		sudo rm -v /home/ark/arkosupdate${RIDGEK_DATE}.zip | tee -a "$LOG_FILE"
 
 		# Use symbolic link so rclone uses default config path, but file is still editable for end-users on EASYROMS partition
 		ln -s /roms/backup/rclone.conf /home/ark/.config/rclone/rclone.conf
+
+		# Grant executable permissions to new scripts
+		sudo chmod -v a+x "/opt/system/Sync Savefiles to Cloud.sh" | tee -a "$LOG_FILE"
+		sudo chmod -v a+x "/opt/system/Advanced/Backup Settings to Cloud.sh" | tee -a "$LOG_FILE"
 	else
 		printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
 		echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
