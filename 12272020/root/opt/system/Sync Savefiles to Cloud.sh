8bed246cf49148a01c0a1f66bad72d34012ddd47
ridgek <ridgekuhn@users.noreply.github.com>
2020-12-28 00:53:08 -0500

    Add parsing retroarch.cfg files for savefile/savestate dirs to "/opt/system/Sync Savefiles to Cloud.sh"

--- 12272020/root/opt/system/Sync Savefiles to Cloud.sh
+++ 12272020/root/opt/system/Sync Savefiles to Cloud.sh
@@ -4,15 +4,6 @@
 ###########
 # PREFLIGHT
 ###########
-# Log file
-LOG_FILE="/roms/backup/rcloud.log"
-
-if [ -f "${LOG_FILE}" ]; then
-	rm "${LOG_FILE}"
-fi
-
-touch "${LOG_FILE}"
-
 # Capture input if script is running standalone
 # (If script was called from "/opt/system/Advanced/Backup Settings to Cloud.sh", the process is already running)
 if [ -z rg351p-js2box ]; then
@@ -22,64 +13,62 @@ if [ -z rg351p-js2box ]; then
 	sudo chmod 777 /dev/input/by-path/platform-odroidgo2-joypad-event-joystick
 fi
 
+LOG_FILE="/roms/backup/rcloud.log"
+RETROARCH_CONFIGS=(/home/ark/.config/retroarch/retroarch.cfg /home/ark/.config/retroarch32/retroarch32.cfg)
+
 # Get user's cloud storage selection
 REMOTE="remote"
 # @todo What's the best way to implement user selection?
 #				A select list would be ideal, instead of having the user type it in
 # REMOTE=`osk "Enter the name of the remote" | tail -n 1`
 
-# @todo These should come from parsing retroarch.cfg
-CONTENT_DIRS_SAVES=true
-CONTENT_DIRS_STATES=true
+# Logging
+if [ -f "${LOG_FILE}" ]; then
+	rm "${LOG_FILE}"
+fi
+
+touch "${LOG_FILE}"
 
 #########################
 # SYNC SAVEFILES TO CLOUD
 #########################
-# Savefiles
-printf "\nSyncing RetroArch savefiles with remote...\n" | tee -a "${LOG_FILE}"
-if [ ${CONTENT_DIRS_SAVES} = true ]; then
-	# Send
-	rclone copy /roms/ ${REMOTE}:retroarch/roms/ --filter-from /roms/backup/rclone/rclone-filters.conf -v | tee -a "${LOG_FILE}"
+# retroarch and retroarch32
+for config in ${RETROARCH_CONFIGS[@]}; do
+	savetypes=(savefile savestate)
 
-	# Receive
-	rclone copy ${REMOTE}:retroarch/roms/ /roms/ --filter-from /roms/backup/rclone/rclone-filters.conf -v | tee -a "${LOG_FILE}"
-else
-	# @todo This should come from parsing retroarch.cfg
-	savesDir="/roms/retroarch/saves"
+	# Loop through savetypes
+	for savetype in ${savetypes[@]}; do
+		# Savefiles
+		savetypes_in_content_dir=$(awk '/^'${savetype}'s_in_content_dir/{ gsub("\"","",$3); print $3}' "${config}")
 
-	# Send
-	rclone copy ${savesDir}/ ${REMOTE}:retroarch/saves/ -v | tee -a "${LOG_FILE}"
+		printf "\nSyncing RetroArch ${savetype}s with ${REMOTE}...\n" | tee -a "${LOG_FILE}"
 
-	# Receive
-	rclone copy ${REMOTE}:retroarch/saves/ ${savesDir}/ -v | tee -a "${LOG_FILE}"
-fi
+		if [ ${savetypes_in_content_dir} = true ]; then
+			# Send
+			rclone copy "/roms/" "${REMOTE}:retroarch/roms/" --filter-from /roms/backup/rclone/rclone-filters.conf -v | tee -a "${LOG_FILE}"
 
-# Savestates
-printf "\nSyncing RetroArch savestates with remote...\n" | tee -a "${LOG_FILE}"
-if [ ${CONTENT_DIRS_STATES} = true ]; then
-	# Send
-	rclone copy /roms/ ${REMOTE}:retroarch/roms/ --filter "+ *.state*" --filter "- *" | tee -a "${LOG_FILE}"
+			# Receive
+			rclone copy "${REMOTE}:retroarch/roms/" "/roms/" --filter-from /roms/backup/rclone/rclone-filters.conf -v | tee -a "${LOG_FILE}"
+		else
+			savetype_directory=$(awk '/^'${savetype}'_directory/{ gsub("\"","",$3); print $3}' "${config}")
 
-	# Receive
-	rclone copy ${REMOTE}:retroarch/roms/ /roms/ --filter "+ *.state*" --filter "- *" | tee -a "${LOG_FILE}"
-else
-	# @todo This should come from parsing retroarch.cfg
-	statesDir="/roms/retroarch/states"
+			# Send
+			rclone copy "${savetype_directory}/" "${REMOTE}:retroarch/${savetype}/" -v | tee -a "${LOG_FILE}"
 
-	# Send
-	rclone copy ${statesDir}/ ${REMOTE}:retroarch/states/ -v | tee -a "${LOG_FILE}"
-
-	# Receive
-	rclone copy ${REMOTE}:retroarch/states/ ${statesDir}/ -v | tee -a "${LOG_FILE}"
-fi
+			# Receive
+			rclone copy "${REMOTE}:retroarch/saves/" "${savetype_directory}/" -v | tee -a "${LOG_FILE}"
+		fi
+	done
+done
 
 # Amiberry Savestates
-printf "\nSyncing Amiberry savestates with remote...\n" | tee -a "${LOG_FILE}"
+printf "\nSyncing Amiberry savestates with ${REMOTE}...\n" | tee -a "${LOG_FILE}"
+
 # Send
-rclone copy /opt/amiberry/savestates/ ${REMOTE}:amiberry/savestates/ -v | tee -a "${LOG_FILE}"
+rclone copy "/opt/amiberry/savestates/" "${REMOTE}:amiberry/savestates/" -v | tee -a "${LOG_FILE}"
 
 # Receive
-rclone copy ${REMOTE}:amiberry/savestates/ /opt/amiberry/savestates/ -v | tee -a "${LOG_FILE}"
+rclone copy "${REMOTE}:amiberry/savestates/" "/opt/amiberry/savestates/" -v | tee -a "${LOG_FILE}"
 
 ##########
 # TEARDOWN
