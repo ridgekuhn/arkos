28d5d5620fdb8b77824942a42c75c93718d86f83
ridgek <ridgekuhn@users.noreply.github.com>
2020-12-27 23:05:49 -0500

    * Cloud sync now uses content dirs (TODO: savefile/savestate paths should come from retroarch.cfg files) * Add rclone-filters.conf * Remove retroarch.cfg overrides

--- 12272020/root/opt/system/Sync Savefiles to Cloud.sh
+++ 12272020/root/opt/system/Sync Savefiles to Cloud.sh
@@ -24,24 +24,61 @@ fi
 
 # Get user's cloud storage selection
 REMOTE="remote"
-
 # @todo What's the best way to implement user selection?
 #				A select list would be ideal, instead of having the user type it in
 # REMOTE=`osk "Enter the name of the remote" | tail -n 1`
 
+# @todo These should come from parsing retroarch.cfg
+CONTENT_DIRS_SAVES=true
+CONTENT_DIRS_STATES=true
+
 #########################
 # SYNC SAVEFILES TO CLOUD
 #########################
+# Savefiles
+printf "\nSyncing RetroArch savefiles with remote...\n" | tee -a "${LOG_FILE}"
+if [ ${CONTENT_DIRS_SAVES} = true ]; then
+	# Send
+	rclone copy /roms/ ${REMOTE}:retroarch/roms/ --filter-from /roms/backup/rclone/rclone-filters.conf -v | tee -a "${LOG_FILE}"
+
+	# Receive
+	rclone copy ${REMOTE}:retroarch/roms/ /roms/ --filter-from /roms/backup/rclone/rclone-filters.conf -v | tee -a "${LOG_FILE}"
+else
+	# @todo This should come from parsing retroarch.cfg
+	savesDir="/roms/retroarch/saves"
+
+	# Send
+	rclone copy ${savesDir}/ ${REMOTE}:retroarch/saves/ -v | tee -a "${LOG_FILE}"
+
+	# Receive
+	rclone copy ${REMOTE}:retroarch/saves/ ${savesDir}/ -v | tee -a "${LOG_FILE}"
+fi
+
+# Savestates
+printf "\nSyncing RetroArch savestates with remote...\n" | tee -a "${LOG_FILE}"
+if [ ${CONTENT_DIRS_STATES} = true ]; then
+	# Send
+	rclone copy /roms/ ${REMOTE}:retroarch/roms/ --filter "+ *.state*" --filter "- *" | tee -a "${LOG_FILE}"
+
+	# Receive
+	rclone copy ${REMOTE}:retroarch/roms/ /roms/ --filter "+ *.state*" --filter "- *" | tee -a "${LOG_FILE}"
+else
+	# @todo This should come from parsing retroarch.cfg
+	statesDir="/roms/retroarch/states"
+
+	# Send
+	rclone copy ${statesDir}/ ${REMOTE}:retroarch/states/ -v | tee -a "${LOG_FILE}"
+
+	# Receive
+	rclone copy ${REMOTE}:retroarch/states/ ${statesDir}/ -v | tee -a "${LOG_FILE}"
+fi
+
+# Amiberry Savestates
+printf "\nSyncing Amiberry savestates with remote...\n" | tee -a "${LOG_FILE}"
 # Send
-printf "\nUploading new savefiles to remote...\n" | tee -a "${LOG_FILE}"
-rclone copy /roms/retroarch/saves/ ${REMOTE}:retroarch/saves/ -v | tee -a "${LOG_FILE}"
-rclone copy /roms/retroarch/states/ ${REMOTE}:retroarch/states/ -v | tee -a "${LOG_FILE}"
 rclone copy /opt/amiberry/savestates/ ${REMOTE}:amiberry/savestates/ -v | tee -a "${LOG_FILE}"
 
 # Receive
-printf "\nDownloading new savefiles from remote...\n" | tee -a "${LOG_FILE}"
-rclone copy ${REMOTE}:retroarch/saves/ /roms/retroarch/saves/ -v | tee -a "${LOG_FILE}"
-rclone copy ${REMOTE}:retroarch/states/ /roms/retroarch/states/ -v | tee -a "${LOG_FILE}"
 rclone copy ${REMOTE}:amiberry/savestates/ /opt/amiberry/savestates/ -v | tee -a "${LOG_FILE}"
 
 ##########
