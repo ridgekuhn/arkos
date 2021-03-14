4bb238a1c3c3c0a4330a9a69d6e93d25e5c6bb89
ridgek <ridgekuhn@users.noreply.github.com>
2021-03-14 11:55:50 -0400

    FIXUP main dialog type error

--- arklone20210118/root/opt/arklone/dialogs/settings.sh
+++ arklone20210118/root/opt/arklone/dialogs/settings.sh
@@ -292,7 +292,7 @@ function homeScreen() {
 # First run dialog
 function firstRunScreen() {
 	# Check if rclone is configured
-	if [ -z $(rclone listremotes 2>/dev/null) ]; then
+	if [ -z "$(rclone listremotes 2>/dev/null)" ]; then
 		whiptail \
 			--title "${TITLE}" \
 			--msgbox "It looks like you haven't configured any rclone remotes yet! Please see the documentation at:\nhttps://github.com/ridgekuhn/arklone\nand\nhttps://rclone.org/docs/" \
